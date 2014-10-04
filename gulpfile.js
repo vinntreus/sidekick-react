ff6e2924717b1fb9297f7cfdc31ccb2c8d1adb65
Daniel Vinntreus <gudski@gmail.com>
2014-10-04 13:56:57 +0200

    develop gulp mode

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
+var nodemon = require('gulp-nodemon');
 
 var baseDir = 'ui';
 var buildDir = './dist/';
@@ -17,6 +18,8 @@ var paths = {
   app_js: ['./' + baseDir + '/js/app.js'],
   js: [baseDir + '/js/*.jsx'],
 };
+
+gulp.task('develop', ['default', 'serve']);
 
 
 gulp.task('build',['css', 'js'], function(){
@@ -31,6 +34,14 @@ gulp.task('build',['css', 'js'], function(){
   .pipe(gulp.dest(buildDir));
 });
 
+//run app using nodemon
+gulp.task('serve', function(){
+  return nodemon({
+    script: 'index.js', //options: '-i ui/*'
+    ignore: ['ui/*', 'dist/*']
+  });
+});
+
 // The default task (called when we run `gulp` from cli)
 gulp.task('default', ['watch', 'css', 'js']);
 
@@ -40,7 +51,7 @@ gulp.task('watch', function() {
   gulp.watch(paths.js, ['js']);
 });
 
-gulp.task('js', ['clean'], function() {
+gulp.task('js', ['clean-js'], function() {
   return browserify(paths.app_js)
     .transform(reactify)
     .bundle()
@@ -48,13 +59,17 @@ gulp.task('js', ['clean'], function() {
     .pipe(gulp.dest(buildDir));
 });
 
-gulp.task('css',['clean'], function() {
+gulp.task('css',['clean-css'], function() {
   return gulp.src(paths.css)
     .pipe(concat('main.css'))
     .pipe(gulp.dest(buildDir));
 });
 
 
-gulp.task('clean', function(done) {
-  del(['dist'], done);
+gulp.task('clean-css', function(done) {
+  del(['dist/*.css'], done);
+});
+
+gulp.task('clean-js', function(done) {
+  del(['dist/*.js'], done);
 });
